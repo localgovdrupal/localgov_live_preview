b2e5ddbdd4c4f754ff84766fbc0c300da36560fe
ekes <ekes@iskra.net>
2025-04-24 15:31:01 +0200

    Cache vary per site (domain). It is already does so by group and group.permissions.
    Also don't error if there is no group (site / domain) context.

--- modules/localgov_live_preview_microsites/src/Plugin/Menu/LivePreviewLocalTask.php
+++ modules/localgov_live_preview_microsites/src/Plugin/Menu/LivePreviewLocalTask.php
@@ -13,18 +13,8 @@ class LivePreviewLocalTask extends LocalTaskDefault {
   public function getRouteParameters(RouteMatchInterface $route_match) {
     $route_parameters = parent::getRouteParameters($route_match);
     $group = localgov_microsites_group_get_by_context();
-    $route_parameters['group'] = $group->id();
+    $route_parameters['group'] = $group?->id();
     return $route_parameters;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getCacheContexts() {
-    if (!isset($this->pluginDefinition['cache_contexts'])) {
-      return [];
-    }
-    return $this->pluginDefinition['cache_contexts'];
   }
 
 }
