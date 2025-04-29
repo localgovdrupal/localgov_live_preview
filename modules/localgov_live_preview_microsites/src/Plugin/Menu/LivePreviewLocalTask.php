cd3c564cb488e09649ee72ae92fd6a5874e10bce
Mark Conroy <mark@mark.ie>
2025-04-29 14:32:16 +0100

    Fix: coding standards

--- modules/localgov_live_preview_microsites/src/Plugin/Menu/LivePreviewLocalTask.php
+++ modules/localgov_live_preview_microsites/src/Plugin/Menu/LivePreviewLocalTask.php
@@ -5,6 +5,13 @@ namespace Drupal\localgov_live_preview_microsites\Plugin\Menu;
 use Drupal\Core\Menu\LocalTaskDefault;
 use Drupal\Core\Routing\RouteMatchInterface;
 
+/**
+ * Provides a local task for Live Preview functionality in microsites.
+ *
+ * This class extends the default local task behavior to include the current
+ * microsite group context in the route parameters, enabling the Live Preview
+ * tab to properly function within the microsite interface.
+ */
 class LivePreviewLocalTask extends LocalTaskDefault {
 
   /**
